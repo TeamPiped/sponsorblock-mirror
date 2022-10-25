cccc095de8829a312b9429d564748adcc0c246ca
Kavin <20838718+FireMasterK@users.noreply.github.com>
2022-10-25 10:13:34 +0100

    Add cors for official instance.

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime};
 
 use diesel::connection::SimpleConnection;
-use rocket::{Build, Rocket};
-use rocket::fairing::AdHoc;
+use rocket::{Build, Request, Response, Rocket};
+use rocket::fairing::{AdHoc, Fairing, Info, Kind};
+use rocket::http::Header;
 use rocket_sync_db_pools::database;
 use tokio::time::interval;
 
@@ -36,6 +37,29 @@ async fn run_migrations(rocket: Rocket<Build>) -> Rocket<Build> {
         }).await;
 
     rocket
+}
+
+pub struct CORS;
+
+#[rocket::async_trait]
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        if request.method() == rocket::http::Method::Options {
+            response.set_streamed_body(tokio::io::empty());
+            response.set_status(rocket::http::Status::Ok);
+        }
+    }
 }
 
 static mut LAST_UPDATE: Option<SystemTime> = None;
@@ -95,5 +119,6 @@ fn rocket() -> Rocket<Build> {
                 });
             })
         })
-        ).mount("/", routes![skip_segments])
+        ).attach(CORS)
+        .mount("/", routes![skip_segments])
 }
